2ec81c6cb88f91f78e11b7766ed2a05331b0b379
User <User@DESKTOP-3APE4MB>
2022-10-13 16:51:45 +0300

    Add project files.

---
+++ 06. Wedding Seats/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06._Wedding_Seats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sector = char.Parse(Console.ReadLine());
+            var redoveSectorA = int.Parse(Console.ReadLine());
+            var mestaNechetenRed = int.Parse(Console.ReadLine());
+            var count = 0;
+            for (int sektor = 'A'; sektor <= sector; sektor++)
+            {
+                for (int red = 1; red <= redoveSectorA; red++)
+                { //да напиша редовете че се качват с 1 
+                    if (red % 2 != 0)
+                    {
+                        for (char mqsto = 'a'; mqsto < 'a' +mestaNechetenRed; mqsto++)
+                        {
+                            Console.WriteLine($"{(char)sektor}{red}{mqsto}");
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        for (char mqsto = 'a'; mqsto < 'a' + mestaNechetenRed +2; mqsto++)
+                        {
+                            Console.WriteLine($"{(char)sektor}{red}{mqsto}");
+                            count++;
+                        }
+
+                    }
+                }
+                redoveSectorA++;
+            }
+            Console.WriteLine(count);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Помощно/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Помощно
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start1 = int.Parse(Console.ReadLine());
+            var start2 = int.Parse(Console.ReadLine());
+            var dif1 = int.Parse(Console.ReadLine()); 
+            var dif2 = int.Parse(Console.ReadLine()); 
+
+            bool isPrime1 = true;
+            var count = 0;
+            bool isPrime2 = true;
+            var count2 = 0;
+
+            for (int first = start1; first <= start1+dif1; first++)
+            {
+                for (int i = 2; i <= first; i++)
+                {
+                    if(first % i == 0)
+                    {
+                        count++;
+                        if(count > 1)
+                        {
+                            isPrime1 = false;
+                            break;
+                        }
+                    }
+                }
+                for (int second = start2; second <= start2+dif2; second++)
+                {
+                    for (int j = 2; j <= second; j++)
+                    {
+                        if(second % j == 0)
+                        {
+                            count2++;
+                            if(count2 > 1)
+                            {
+                                isPrime2 = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isPrime1 && isPrime2)
+                    {
+                        Console.WriteLine($"{first}{second}");
+                    }
+                    count = 0;
+                    count2 = 0;
+                    isPrime2 = true;
+
+                }
+                isPrime1 = true;
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
