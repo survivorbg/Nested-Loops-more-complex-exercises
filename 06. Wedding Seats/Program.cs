81bac0d5a4545678e848c71f859bcbc96ff5048c
Vladimir Petrov <70769278+survivorbg@users.noreply.github.com>
2022-10-13 14:07:32 +0000

    Update Program.cs

--- 06. Wedding Seats/Program.cs
+++ 06. Wedding Seats/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _06._Wedding_Seats
 {
@@ -6,42 +6,35 @@ namespace _06._Wedding_Seats
     {
         static void Main(string[] args)
         {
-
             var sector = char.Parse(Console.ReadLine());
-            var redoveSectorA = int.Parse(Console.ReadLine());
-            var mestaNechetenRed = int.Parse(Console.ReadLine());
+            var rowsSectorA = int.Parse(Console.ReadLine());
+            var seatsOddRow = int.Parse(Console.ReadLine());
             var count = 0;
             for (int sektor = 'A'; sektor <= sector; sektor++)
             {
-                for (int red = 1; red <= redoveSectorA; red++)
-                { //да напиша редовете че се качват с 1 
-                    if (red % 2 != 0)
+                for (int row = 1; row <= rowsSectorA; row++)
+                {  
+                    if (row % 2 != 0)
                     {
-                        for (char mqsto = 'a'; mqsto < 'a' +mestaNechetenRed; mqsto++)
+                        for (char seat = 'a'; seat < 'a' +seatsOddRow; seat++)
                         {
-                            Console.WriteLine($"{(char)sektor}{red}{mqsto}");
+                            Console.WriteLine($"{(char)sektor}{row}{seat}");
                             count++;
                         }
                     }
                     else
                     {
-                        for (char mqsto = 'a'; mqsto < 'a' + mestaNechetenRed +2; mqsto++)
+                        for (char seat = 'a'; seat < 'a' + seatsOddRow +2; seat++)
                         {
-                            Console.WriteLine($"{(char)sektor}{red}{mqsto}");
+                            Console.WriteLine($"{(char)sektor}{row}{seat}");
                             count++;
                         }
 
                     }
                 }
-                redoveSectorA++;
+                rowsSectorA++;
             }
             Console.WriteLine(count);
-
-
-
-
-
-
         }
     }
 }
